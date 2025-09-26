320ffba13e5866fa86e7b63bc7153226dfe8be0f
perlytiara <overflowedimagination@gmail.com>
2025-09-26 14:13:57 +0200

    Release v1.0.9: Target version for auto-update testing + Fix duplicate workflow builds

--- src/components/UpdateModal.svelte
+++ src/components/UpdateModal.svelte
@@ -7,7 +7,7 @@
   export let isDownloading = false;
   export let isChecking = false;
   export let error = null;
-  export let currentVersion = '1.0.8';
+  export let currentVersion = '1.0.9';
   
   const dispatch = createEventDispatcher();
   
