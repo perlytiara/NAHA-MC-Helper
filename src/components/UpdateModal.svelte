34771abfd1162a6899b1f0dee73dd7666726be3f
perlytiara <overflowedimagination@gmail.com>
2025-09-27 08:42:51 +0200

    Update version to 1.1.9 - Additional auto-update testing version

--- src/components/UpdateModal.svelte
+++ src/components/UpdateModal.svelte
@@ -7,7 +7,7 @@
   export let isDownloading = false;
   export let isChecking = false;
   export let error = null;
-  export let currentVersion = '1.1.8';
+  export let currentVersion = '1.1.9';
   
   const dispatch = createEventDispatcher();
   
